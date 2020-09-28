0130ed1232641c611c57c03be77568e24cd069b1
hannayatsevich <hannayatsevich@gmail.com>
2020-09-28 22:04:55 +0300

    feat: implement  func for searching min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0) return 0;
+  return array.reduce( (acc, elem) => Math.min(acc, elem), 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0) return 0;
+  return array.reduce( (acc, elem) => Math.max(acc, elem), 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length === 0) return 0;
+  return array.reduce( (acc, elem) => acc +elem, 0) / array.length;
 }
